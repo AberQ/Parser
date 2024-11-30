97ae85fb939b11ae606ad5929486312fab13b243
AberQ <egor.master2017@gmail.com>
2024-11-30 19:23:41 +0500

    Добавил выведение названий продуктов

--- first_parser.py
+++ first_parser.py
@@ -6,6 +6,6 @@ html = BS(metro_site.content, 'html.parser')
 
 
 
-for element in html.select(".subcategory-or-type__products > .catalog-2-level-product-card product-card subcategory-or-type__products-item with-prices-drop"):
+for element in html.select(".subcategory-or-type__products > .catalog-2-level-product-card"):
     title = element.select(".caption > a")
-    print( title[0].text)
+    print( title)
