e6da84f42fd374cb5c8dd012c2f29fc9b2ab8560
AberQ <egor.master2017@gmail.com>
2024-11-30 23:16:39 +0500

    Изменение названий под требование тз

--- second_parser.py
+++ second_parser.py
@@ -1,13 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 
-# Базовый URL для пагинации
+
 base_url = "https://online.metro-cc.ru/category/rybnye/ohlazhdennaya-ryba?page={}"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
 }
 
-# Функция для парсинга одной страницы
+
 def parse_page(page_number):
     url = base_url.format(page_number)
     response = requests.get(url, headers=headers)
@@ -15,7 +15,7 @@ def parse_page(page_number):
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
         
-        # Поиск контейнера с продуктами
+        # Поиск контейнера(в том числе id и наименования)
         products_container = soup.find("div", id="products-inner")
         if products_container:
             product_cards = products_container.find_all("div", class_="catalog-2-level-product-card product-card subcategory-or-type__products-item with-prices-drop")
@@ -44,24 +44,24 @@ def parse_page(page_number):
                     actual_price_rubles = actual_price_sum.find("span", class_="product-price__sum-rubles") if actual_price_sum else None
                     actual_price = actual_price_rubles.text.strip() if actual_price_rubles else "Цена отсутствует"
 
-                print(f"ID: {product_id}, Название: {product_name}, Ссылка: {full_product_link}, Регулярная цена: {old_price}, Промо-цена: {actual_price}")
+                print(f"id товара: {product_id}, Наименование: {product_name}, Ссылка на товар: {full_product_link}, Регулярная цена: {old_price}, Промо цена: {actual_price}")
         
         return len(product_cards) if products_container else 0
-        
+        #Парсер будет бесконечно искать новые страницы на сайте МЕТРО пока не наткнется на ошибку 404
     elif response.status_code == 404:
         print(f"Ошибка 404: Страница {page_number} не найдена.")
-        return -1  # Возвращаем -1, чтобы остановить цикл на ошибке 404
+        return -1  
     else:
         print(f"Ошибка {response.status_code} на странице {page_number}.")
         return 0
 
-# Цикл по всем страницам
+
 total_products = 0
 page = 1
 while True:
     print(f"Парсинг страницы {page}...")
     found_products = parse_page(page)
-    if found_products == -1:  # Останавливаемся на ошибке 404
+    if found_products == -1: 
         break
     total_products += found_products
     page += 1
